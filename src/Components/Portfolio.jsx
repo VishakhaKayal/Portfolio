82f74323f24b2d354f18ca24803289a7a0eeef2c
Vishakha-Kayal <138288780+Vishakha-Kayal@users.noreply.github.com>
2024-08-06 23:29:27 +0530

    added link to the project

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -8,7 +8,7 @@ const Portfolio = () => {
       <div className="w-full h-screen flex items-center">
         <div
           className="text-white bg-[#101010] w-full h-[calc(100vh-35vh)] flex flex-col gap-4 z-99 relative"
-          id="about"
+          id="portfolio"
         >
           <h2 className="text-5xl font-bold ">My Works</h2>
           <div className=" flex justify-center gap-8  w-full h-full overflow-hidden">
@@ -18,8 +18,8 @@ const Portfolio = () => {
               return ( // Add return statement here
                <>
                 <div className="w-[30%] h-full rounded-xl overflow-hidden relative" key={i}>
-                    <a href={work.hostlink} target="_blank"><img src={work.image} alt="" className="w-full h-full" /></a>
-                    <div className="w-full h-full absolute top-0 bg-[#0000006c]"></div>
+                    <img src={work.image} alt="" className="w-full h-full" />
+                    <a href={work.hostlink} target="_blank"><div className="w-full h-full absolute top-0 bg-[#0000006c]"></div></a>
                 </div>
                </>
               );
