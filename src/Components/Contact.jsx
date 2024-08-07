c4550bed30e94387900c0a70c996bc5900a251d2
Vishakha-Kayal <138288780+Vishakha-Kayal@users.noreply.github.com>
2024-08-07 10:42:50 +0530

    added contact message

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,6 +1,8 @@
-import React, { useRef, useState,useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Button from "./Button";
 import emailjs from 'emailjs-com';
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Contact = () => {
   const submitRef = useRef();
@@ -14,9 +16,15 @@ const Contact = () => {
     e.preventDefault();
     emailjs.sendForm('service_zggpenr', 'template_le5bhop', submitRef.current)
       .then((result) => {
-        console.log(result.text);
+        // console.log(result.text);
+        toast.success("Contact details send Successfully");
       }, (error) => {
-        console.log(error.text);
+        if (error.text == "The recipients address is corrupted")
+          toast.error("please enter valid email")
+        else {
+          toast.error("something went wrong")
+        }
+        // console.log(error.text);
       });
   };
   const [name, setName] = useState("")
@@ -24,44 +32,47 @@ const Contact = () => {
   const [message, setMessage] = useState("")
 
   return (
-    <div className="w-full h-[100vh] flex items-center">
-      <div
-        className="text-white w-full h-[50vh]  flex flex-col items-center"
-        id="contact"
-      >
-        <div className="w-full text-3xl font-[Poppins] mb-4 flex justify-center">
-          Contact Me
+    <>
+      <ToastContainer />
+      <div className="w-full h-[100vh] flex items-center">
+        <div
+          className="text-white w-full h-[50vh]  flex flex-col items-center"
+          id="contact"
+        >
+          <div className="w-full text-3xl font-[Poppins] mb-4 flex justify-center">
+            Contact Me
+          </div>
+          <form ref={submitRef} className="w-full flex flex-col items-center">
+            <input
+              type="text"
+              name="user_name"
+              placeholder="Your Name"
+              className="w-[60%] px-3 py-3 mb-3 bg-[#282828] rounded-xl"
+              value={name}
+              onChange={(e) => { setName(e.target.value) }}
+            />
+            <input
+              type="email"
+              name="user_email"
+              placeholder="Your Email"
+              className="w-[60%]  px-3 py-3 mb-3 bg-[#282828] rounded-xl"
+              value={email}
+              onChange={(e) => { setEmail(e.target.value) }}
+            />
+            <textarea
+              name="message"
+              id=""
+              placeholder="Your Message"
+              rows="4"
+              className="w-[60%] px-3 py-3 mb-3 bg-[#282828] rounded-xl"
+              value={message}
+              onChange={(e) => { setMessage(e.target.value) }}
+            ></textarea>
+            <Button name="Submit" handleButton={handleButton} />
+          </form>
         </div>
-        <form ref={submitRef} className="w-full flex flex-col items-center">
-          <input
-            type="text"
-            name="user_name"
-            placeholder="Your Name"
-            className="w-[60%] px-3 py-3 mb-3 bg-[#282828] rounded-xl"
-            value={name}
-            onChange={(e) => { setName(e.target.value) }}
-          />
-          <input
-            type="email"
-            name="user_email"
-            placeholder="Your Email"
-            className="w-[60%]  px-3 py-3 mb-3 bg-[#282828] rounded-xl"
-            value={email}
-            onChange={(e) => { setEmail(e.target.value) }}
-          />
-          <textarea
-            name="message"
-            id=""
-            placeholder="Your Message"
-            rows="4"
-            className="w-[60%] px-3 py-3 mb-3 bg-[#282828] rounded-xl"
-            value={message}
-            onChange={(e) => { setMessage(e.target.value) }}
-          ></textarea>
-          <Button name="Submit" handleButton={handleButton} />
-        </form>
       </div>
-    </div>
+    </>
   );
 };
 
