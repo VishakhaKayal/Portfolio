b71a5838be2c85fa7d8260ba58c8de5e06f64c67
VishakhaKayal <mgpsvishakha7944@gmail.com>
2024-08-09 23:53:32 +0530

    added some animations

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -2,17 +2,18 @@ import React from "react";
 
 const Project = ({ project }) => {
   return (
-    <div
-      className="w-[30%] h-[40vh] rounded-xl overflow-hidden relative"
-    
-    >
-      <div className="absolute top-0 w-full h-full">
-        <img src={project.image} alt="" className="w-full h-full" />
-      </div>
+    <div className="w-[30%] h-[40vh] rounded-xl overflow-hidden relative">
+      <a href={project.hostlink} target="_blank">
+        <div className="absolute top-0 w-full h-full">
+          <img src={project.image} alt="" className="w-full h-full" />
+        </div>{" "}
+      </a>
       <a href={project.hostlink} target="_blank">
         <div className="projectDiv w-full h-full absolute top-0 bg-[#000000a0] flex flex-col justify-center items-center">
-            <h3 className="capitalize  text-xl font-semibold">{project.category}</h3>
-           <p className=" text-white"> Click Me</p>
+          <h3 className="capitalize  text-xl font-semibold">
+            {project.category}
+          </h3>
+          <p className=" text-white"> Click Me</p>
         </div>
       </a>
     </div>
